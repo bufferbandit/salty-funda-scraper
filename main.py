298bc7db0de6f03ca88778f66e2a37ba212edc62
bufferbandit <kernelland@protonmail.ch>
2024-04-24 00:58:46 +0200

    __init__

---
+++ main.py
@@ -0,0 +1,129 @@
+from HLISA.hlisa_action_chains import HLISA_ActionChains
+
+from helium import start_chrome, start_firefox, set_driver, click
+import undetected_chromedriver as uc
+from helium import Button, Text, TextField
+from time import sleep
+
+from urllib.parse import urlencode
+from selenium_functions import *
+
+from selenium.webdriver.common.by import By
+
+
+# auto add selenium
+
+
+
+# Formatting functions
+range_to_str = lambda r: f"\"{r[0]}-{r[1]}\""
+list_to_list_repr = lambda l: str(l).replace('\'', "\"")
+
+
+# koop|huur
+def build_search_url(
+		form_of_living: str,
+		selected_area: list[str] = None,
+		object_type: list[str] = None,
+		price_range: tuple[int, int] = None,
+		publication_date: int = None,
+		availability: list[str] = None,
+		floor_area_range: tuple[int, int] = None,
+		plot_area_range: tuple[int, int] = None,
+		rooms_range: tuple[int, int] = None,
+		bedrooms_range: tuple[int, int] = None,
+		energy_label: list[str] = None,
+		exterior_space_type: list[str] = None,
+		exterior_space_garden_orientation: list[str] = None,
+		exterior_space_garden_size_range: tuple[int, int] = None,
+		construction_type: list[str] = None,
+		zoning: list[str] = None,
+		free_text_search: list[str] = None,
+		construction_period: list[str] = None,
+		garage_capacity_range: tuple[int, int] = None,
+		amenities: list[str] = None,
+		type: list[str] = None,
+		open_house: list[str] = None,
+):
+	base_url = f"https://www.funda.nl/zoeken/{form_of_living}?"
+
+	params = {}
+
+	if selected_area:
+		params["selected_area"] = list_to_list_repr(selected_area)
+	if object_type:
+		params["object_type"] = list_to_list_repr(object_type)
+	if price_range:
+		params["price"] = range_to_str(price_range)
+	if publication_date:
+		params["publication_date"] = range_to_str(publication_date)
+	if availability:
+		params["availability"] = list_to_list_repr(availability)
+	if floor_area_range:
+		params["floor_area"] = range_to_str(floor_area_range)
+	if plot_area_range:
+		params["plot_area"] = range_to_str(plot_area_range)
+	if rooms_range:
+		params["rooms"] = range_to_str(rooms_range)
+	if bedrooms_range:
+		params["bedrooms"] = range_to_str(bedrooms_range)
+	if energy_label:
+		params["energy_label"] = list_to_list_repr(energy_label)
+	if exterior_space_type:
+		params["exterior_space_type"] = list_to_list_repr(exterior_space_type)
+	if exterior_space_garden_orientation:
+		params["exterior_space_garden_orientation"] = list_to_list_repr(exterior_space_garden_orientation)
+	if exterior_space_garden_size_range:
+		params["exterior_space_garden_size"] = range_to_str(exterior_space_garden_size_range)
+	if construction_type:
+		params["construction_type"] = list_to_list_repr(construction_type)
+	if zoning:
+		params["zoning"] = list_to_list_repr(zoning)
+	if free_text_search:
+		params["free_text_search"] = list_to_list_repr(free_text_search)
+	if construction_period:
+		params["construction_period"] = list_to_list_repr(construction_period)
+	if garage_capacity_range:
+		params["garage_capacity"] = range_to_str(garage_capacity_range)
+	if amenities:
+		params["amenities"] = list_to_list_repr(amenities)
+	if type:
+		params["type"] = list_to_list_repr(type),
+	if open_house:
+		params["open_house"] = list_to_list_repr(open_house)
+
+	# "https://www.funda.nl/zoeken/koop?selected_area=["amsterdam"]&object_type=["house"]&price="75000-450000"&publication_date="1"&availability=["available","negotiations","unavailable"]&floor_area="50-250"&plot_area="250-5000"&rooms="1-5"&bedrooms="1-5"&energy_label=["A+++++","D","A++++"]&exterior_space_type=["balcony","terrace"]&exterior_space_garden_orientation=["east","north"]&exterior_space_garden_size="25-500"&construction_type=["newly_built","resale"]&zoning=["recreational","residential"]&free_text_search=["warmtepomp","dakteras"]&construction_period=["before_1906","from_1906_to_1930"]&garage_capacity="1-5"&amenities=["bathtub","central_heating_boiler"]&type=["single","group"]&open_house=["all","today","coming_weekend"]"
+
+	return base_url + urlencode(params)
+
+
+
+
+
+def main_flow(driver, hla):
+	# Go to the home
+	driver.get("https://funda.nl")
+	sleep(1)
+
+	# Reject cookies
+	reject_cookies(hla)
+
+	# Go to the search page
+	area = ["rotterdam"]
+	search_url = build_search_url("koop", area)
+
+	process_paginated(driver, search_url)
+
+
+	pass
+	input()
+
+
+if __name__ == "__main__":
+	# Setup
+	driver = uc.Chrome(headless=False)
+	hla = HLISA_ActionChains(driver)
+	set_driver(driver)
+
+	# Browsing
+	main_flow(driver, hla)
---
+++ selenium_functions.py
@@ -0,0 +1,123 @@
+from selenium.webdriver.common.by import By
+from helium import Button, TextField
+from urllib.parse import urlencode
+from time import sleep
+
+
+
+def reject_cookies(hla):
+	if (alles_weigeren := Button("Alles weigeren")).exists():
+		sleep(1)
+		hla \
+			.move_to_element(alles_weigeren.web_element) \
+			.click() \
+			.perform()
+
+
+def search(hla, search_text):
+	searchbox = TextField("Zoek op plaats, buurt of postcode")
+	if searchbox.exists():
+		searchbox_element = searchbox.web_element
+		hla \
+			.send_keys(search_text, searchbox_element)\
+			.perform()
+
+def get_results_items(driver):
+	results_items_container = driver.find_element(By.CLASS_NAME, "pagination")
+	results_items_elements = results_items_container \
+		.find_elements(By.TAG_NAME, "li")
+	results_items = [results_items_element.text for results_items_element in results_items_elements]
+	return results_items
+
+
+def get_max_items(driver):
+	results_items = get_results_items(driver)
+	max_item = int(results_items[-2])
+	return max_item
+
+def process_all_process_searchresults_pages(driver, base_url, max_pages, start=2):
+	for x in range(start, max_pages+1):
+		paginated_url = base_url + "&" + urlencode({"search_result": x})
+		yield process_individual_searchresults_page(driver, paginated_url)
+
+
+
+def process_paginated(driver, original_url):
+	# Visit the search url
+	driver.get(original_url)
+
+	# Process the first page
+	first_page_results = process_individual_searchresults_page(driver, original_url)
+	# Get the max number of pages
+	max_items = get_max_items(driver)
+
+	# And now we know how many pages are left we can request the rest of them,
+	# without rerequesting the first one which would be wasteful
+	other_pages_results = list(process_all_process_searchresults_pages(driver, original_url, max_items, 2))
+
+
+	return first_page_results + other_pages_results
+
+def process_individual_searchresults_page(driver, url=None, shallow_scrape=False):
+
+	page_results = []
+
+	if url:driver.get(url)
+
+	# If shallow scrape only scrape the info that is on the cards
+	# card container
+	search_result_items = driver.find_elements(By.XPATH, '//div[@data-test-id="search-result-item"]')
+	for search_result_cards in search_result_items:
+		card_data = process_search_result_item(search_result_cards)
+		print(card_data)
+		page_results.append(card_data)
+
+	if not shallow_scrape:
+
+		# Scrape an individual page
+
+		# And add the results to the more detailed results
+
+		...
+
+
+	# Otherwise visit the urls themselves
+	return page_results
+
+
+def process_search_result_item(search_result_element):
+	street_name_house_number = search_result_element.find_element(By.XPATH, './/h2[@data-test-id="street-name-house-number"]').text
+	postal_code_city = search_result_element.find_element(By.XPATH, './/div[@data-test-id="postal-code-city"]').text
+	price = search_result_element.find_element(By.XPATH, './/p[@data-test-id="price-sale"]').text
+	url = search_result_element.find_element(By.XPATH, './/a[@data-test-id="object-image-link"]').get_attribute("href")
+
+	tag_data = search_result_element.find_elements(By.TAG_NAME, "li")
+	if len(tag_data) == 3:
+		home_scurface = tag_data[0].text
+		percel_scurface = None
+		bedrooms = tag_data[1].text
+		energy_label = tag_data[2].text
+	elif len(tag_data) == 4:
+		home_scurface = tag_data[1].text
+		percel_scurface = None
+		bedrooms = tag_data[2].text
+		energy_label = tag_data[3].text
+	elif len(tag_data) == 5:
+		home_scurface = tag_data[1].text
+		percel_scurface = tag_data[2].text
+		bedrooms = tag_data[3].text
+		energy_label = tag_data[4].text
+	else:
+		raise Exception(f"Unexpected number of tags found: {len(tag_data)}" )
+
+
+	return [
+		street_name_house_number,
+		postal_code_city,
+		price,
+		url,
+		home_scurface,
+		percel_scurface,
+		bedrooms,
+		energy_label
+	]
